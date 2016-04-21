8474428f869ffa89341a240e48a141676fe232da
trovsankar <kalai.b@trov.co.in>
2016-04-21 11:04:54 +0530

    Updated script resource in db

--- bm-db-vanilla/recipes/default.rb
+++ bm-db-vanilla/recipes/default.rb
@@ -32,7 +32,7 @@
 #databag_item.save
 #########################################################
 
-$public_ip = `wget http://ipinfo.io/ip -qO -`
+$public_ip = `wget http://ipecho.net/plain -O - -q ; echo`
 #puts "#$public_ip"
 # #To edit the contents of a data bag item from a recipe:
 database_ip = data_bag_item('sankars', 'multi_info')
@@ -74,51 +74,21 @@ end
 ########################################################################################################################
 log "*** Setting up the application user and database. ***"
 
-########################################################################################################################
-# Create the database
-########################################################################################################################
-execute "create_database" do
-    command "mysqladmin create vanilla"
-end
 
-########################################################################################################################
-# Load the the database with some default data
-########################################################################################################################
-execute "load_database" do
-    command "mysql -uroot vanilla < /tmp/bm_vanilla.sql"
-    #only_if { !node.default['config']['mysql_database_load_file'].nil? && node.default['config']['mysql_database_load_file'] != "" }
-end
-
-########################################################################################################################
-# Grant access to the database to the application user
-########################################################################################################################
-execute "grant_access" do
-    command "mysql -uroot -e \"GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON vanilla.* TO 'vanilla'@'%' IDENTIFIED BY 'PassWord';\""
-end
-
-
-execute "bind_address" do
-    command "sudo sed -e '/bind-address/s/^/#/g' -i /etc/mysql/my.cnf"
-end
-
-########################################################################################################################
-# Reload the privileges.
-########################################################################################################################
-execute "flush_priviliges" do
-    command "mysqladmin flush-privileges"
-end
-
-######################################################################################################################################################
-# Open up Port 3306 (MySQL).
-######################################################################################################################################################
-execute "iptables -I INPUT -p tcp --dport 3306 -j ACCEPT"
-execute "iptables-save"
-
-####################################################################################
-## Restart mysql service
-####################################################################################
-execute "mysql_service_restart" do
-    command "sudo /etc/init.d/mysql restart"
+script "database_load" do
+  interpreter "bash"
+  #user "root"
+  cwd "/tmp"
+  code <<-EOF
+		mysqladmin create vanilla
+		mysql -uroot vanilla < /tmp/bm_vanilla.sql
+		mysql -uroot -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON vanilla.* TO 'vanilla'@'%' IDENTIFIED BY 'PassWord';"
+		sudo sed -e '/bind-address/s/^/#/g' -i /etc/mysql/my.cnf
+		mysqladmin flush-privileges
+		iptables -I INPUT -p tcp --dport 3306 -j ACCEPT
+		iptables-save
+		sudo /etc/init.d/mysql restart
+	EOF
 end
 
 
